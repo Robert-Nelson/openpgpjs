a2a08ffe1f21626613a8a7d1629abe2e6accdf87
Robert Nelson <robertn@the-nelsons.org>
2014-01-19 00:01:11 -0800

    fix git config

--- travis/success.sh
+++ travis/success.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "${TRAVIS_BRANCH}" == "master" -o true ]; then
-    git config user.name="Travis Build"
-    git config user.email="nobody@travis-ci.org"
+    git config user.name "Travis Build"
+    git config user.email "nobody@travis-ci.org"
     git tag --force -m "Latest tested version" LatestDev "${TRAVIS_COMMIT}"
     node travis/make-release.js
 fi
