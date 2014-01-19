bee72abd9c821a7137aea9a33f572f3fd0131f08
Robert Nelson <robertn@the-nelsons.org>
2014-01-18 23:31:40 -0800

    fix make-release call

--- travis/success.sh
+++ travis/success.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 if [ "${TRAVIS_BRANCH}" == "master" -o true ]; then
+    echo TRAVIS_BRANCH=${TRAVIS_BRANCH}
+    echo TRAVIS_COMMIT=${TRAVIS_COMMIT}
     git tag --force -m "Latest tested version" LatestDev "${TRAVIS_COMMIT}"
-    travis/make-release
+    node travis/make-release.js
 fi
