378157d1ec449a8415d026059bad67def684065f
Robert Nelson <robertn@the-nelsons.org>
2014-01-19 15:43:36 -0800

    Fix username

--- travis/success.sh
+++ travis/success.sh
@@ -6,7 +6,7 @@ if [ "${TRAVIS_NODE_VERSION}" == "0.10" ]; then
         export GIT_ASKPASS=/bin/true
         git config user.name "Travis Build"
         git config user.email "nobody@travis-ci.org"
-        git config credential.https://github.com.username=${GITHUB_TOKEN}
+        git config credential.https://github.com.username ${GITHUB_TOKEN}
         git tag --force -m "Latest tested version" LatestDev "${TRAVIS_COMMIT}"
         git push
         cd ../..
