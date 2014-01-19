9a79aa49b3039a8f3c4c6fdf8b8e4d020ceafd83
Robert Nelson <robertn@the-nelsons.org>
2014-01-19 15:35:16 -0800

    Checkout repo for updating

--- travis/success.sh
+++ travis/success.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 if [ "${TRAVIS_NODE_VERSION}" == "0.10" ]; then
     if [ "${TRAVIS_BRANCH}" == "master" -o true ]; then
+        git clone --branch ${TRAVIS_BRANCH} https://github.com/${TRAVIS_REPO_SLUG}.git travis/github
+        cd travis/github
+        export GIT_ASKPASS=/bin/true
         git config user.name "Travis Build"
         git config user.email "nobody@travis-ci.org"
+        git config credential.https://github.com.username=${GITHUB_TOKEN}
         git tag --force -m "Latest tested version" LatestDev "${TRAVIS_COMMIT}"
         git push
+        cd ../..
+        rm -rf travis/github
         node travis/make-release.js
     fi
 fi
