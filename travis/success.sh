8d07c737bdd10172ee2eb8c7446fe487d8a035fe
Robert Nelson <robertn@the-nelsons.org>
2014-01-19 14:09:40 -0800

    Try annotated tag

--- travis/success.sh
+++ travis/success.sh
@@ -3,7 +3,7 @@ if [ "${TRAVIS_NODE_VERSION}" == "0.10" ]; then
     if [ "${TRAVIS_BRANCH}" == "master" -o true ]; then
         git config user.name "Travis Build"
         git config user.email "nobody@travis-ci.org"
-        git tag --force -m "Latest tested version" LatestDev "${TRAVIS_COMMIT}"
+        git tag --force -a -m "Latest tested version" LatestDev "${TRAVIS_COMMIT}"
         node travis/make-release.js
     fi
 fi
