5b31095128e14a1eb7fa2ad428ffbc9650ffdd0d
Robert Nelson <robertn@the-nelsons.org>
2014-01-19 20:52:08 -0800

    Update tag using github API

--- travis/make-release.js
+++ travis/make-release.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var Client = require('github');
 
 var repoPath = process.env['TRAVIS_REPO_SLUG'].split('/');
+var commitSHA = process.env["GITHUB_COMMIT"];
 
 var owner = repoPath[0];
 var repo = repoPath[1];
@@ -32,83 +33,115 @@ client.authenticate(
   }
 );
 
-client.repos.getAllReleases(
+client.gitdata.updateReference(
   {
     "owner": release.owner,
-    "repo": release.repo
+    "repo": release.repo,
+    "ref": "tags/" + release.tag_name,
+    "sha": commitSHA
   },
   function (err, res) {
-    if (!err) {
-      var processed = false;
-      res.forEach(function (item) {
-        if (item.tag_name == "LatestDev") {
-          if (!processed) {
-            processed = true;
-            release.id = item.id;
-            client.repos.editRelease(
-              release,
-              function (err, res) {
-                if (!err) {
-                  var assetCount = item.assets.length;
-                  if (assetCount > 0) {
-                    item.assets.forEach(function (asset) {
-                      client.repos.deleteReleaseAsset(
-                        {
-                          "owner": release.owner,
-                          "repo": release.repo,
-                          "id": asset.id
-                        },
-                        function (err, res) {
-                          if (--assetCount <= 0) {
-                            uploadAssets(client, pkg, release);
-                          }
-                        }
-                      );
-                    });
-                  } else {
-                    uploadAssets(client, pkg, release);
-                  }
-                } else {
-                  console.log("repos.editRelease:\n", err);
-                }
-              }
-            );
+    if (err) {
+      client.gitdata.createReference(
+        {
+          "owner": release.owner,
+          "repo": release.repo,
+          "ref": "refs/tags/" + release.tag_name,
+          "sha": commitSHA
+        },
+        function (err, res) {
+          if (!err) {
+            createRelease();
           } else {
-            client.repos.deleteRelease(
-              {
-                "owner": release.owner,
-                "repo": release.repo,
-                "id": item.id
-              },
-              function (err, res) {
-                if (err) {
-                  console.log("repos.deleteRelease:\n", err);
-                }
-              }
-            );
+            console.log("repos.createReference:\n", err);
           }
         }
-      });
-      if (!processed) {
-        client.repos.createRelease(
-          release,
-          function (err, res) {
-            if (!err) {
-              release.id = res.id;
-              uploadAssets(client, pkg, release);
-            } else {
-              console.log("repos.createRelease:\n", err);
-            }
-          }
-        );
-      }
+      );
     } else {
-      console.log("repos.getAllReleases:\n", err);
+      createRelease();
     }
   }
 );
 
-function uploadAssets(client, pkg, release) {
+function createRelease() {
+  client.repos.getAllReleases(
+    {
+      "owner": release.owner,
+      "repo": release.repo
+    },
+    function (err, res) {
+      if (!err) {
+        var processed = false;
+        res.forEach(function (item) {
+          if (item.tag_name == "LatestDev") {
+            if (!processed) {
+              processed = true;
+              release.id = item.id;
+              client.repos.editRelease(
+                release,
+                function (err, res) {
+                  if (!err) {
+                    var assetCount = item.assets.length;
+                    if (assetCount > 0) {
+                      item.assets.forEach(function (asset) {
+                        client.repos.deleteReleaseAsset(
+                          {
+                            "owner": release.owner,
+                            "repo": release.repo,
+                            "id": asset.id
+                          },
+                          function (err, res) {
+                            if (--assetCount <= 0) {
+                              uploadAssets(client, pkg, release);
+                            }
+                          }
+                        );
+                      });
+                    } else {
+                      uploadAssets(client, pkg, release);
+                    }
+                  } else {
+                    console.log("repos.editRelease:\n", err);
+                  }
+                }
+              );
+            } else {
+              client.repos.deleteRelease(
+                {
+                  "owner": release.owner,
+                  "repo": release.repo,
+                  "id": item.id
+                },
+                function (err, res) {
+                  if (err) {
+                    console.log("repos.deleteRelease:\n", err);
+                  }
+                }
+              );
+            }
+          }
+        });
+        if (!processed) {
+          client.repos.createRelease(
+            release,
+            function (err, res) {
+              if (!err) {
+                release.id = res.id;
+                uploadAssets(client, pkg, release);
+              } else {
+                console.log("repos.createRelease:\n", err);
+              }
+            }
+          );
+        }
+      } else {
+        console.log("repos.getAllReleases:\n", err);
+      }
+    }
+  );
+}
+
+function uploadAssets() {
   [ "openpgp.min.js", pkg.name + "-" + pkg.version + ".tgz" ].forEach(function (asset) {
     client.repos.uploadReleaseAsset(
       {
